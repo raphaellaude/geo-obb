9508d38390f0c3b1a61b48626a91c546d6df45bf
Raphael Paul Laude <raphlaude@gmail.com>
2023-07-14 13:41:23 -0400

    testing diff ideas

--- geoobb/obb.py
+++ geoobb/obb.py
@@ -33,16 +33,16 @@ def oriented_bounding_box(pts: np.ndarray) -> np.ndarray:
     """
     ca = np.cov(pts, y=None, rowvar=False, bias=True)
 
-    v, vect = np.linalg.eig(ca)
+    val, vect = np.linalg.eig(ca)
     tvect = np.transpose(vect)
 
     #use the inverse of the eigenvectors as a rotation matrix and
     #rotate the points so they align with the x and y axes
-    ar = np.dot(pts, np.linalg.inv(tvect))
+    arr = np.dot(pts, np.linalg.inv(tvect))
 
     # get the minimum and maximum x and y
-    mina = np.min(ar, axis=0)
-    maxa = np.max(ar, axis=0)
+    mina = np.min(arr, axis=0)
+    maxa = np.max(arr, axis=0)
     diff = (maxa - mina) * 0.5
 
     # the center is just half way between the min and max xy
@@ -61,3 +61,24 @@ def oriented_bounding_box(pts: np.ndarray) -> np.ndarray:
     center = np.dot(center, tvect)
 
     return corners
+
+
+def oriented_bb_center_diff(pts: np.ndarray):
+    """
+    """
+    val, vect = np.linalg.eig(np.cov(pts, y=None, rowvar=False, bias=True))
+    tvect = np.transpose(vect)
+
+    #use the inverse of the eigenvectors as a rotation matrix and
+    #rotate the points so they align with the x and y axes
+    arr = np.dot(pts, np.linalg.inv(tvect))
+
+    # get the minimum and maximum x and y
+    mina = np.min(arr, axis=0)
+    maxa = np.max(arr, axis=0)
+    diff = (maxa - mina) * 0.5
+
+    # the center is just half way between the min and max xy
+    center = mina + diff
+
+    return center, diff
